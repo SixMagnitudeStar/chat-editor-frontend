6f17f6ef6ee19fe3748d6ae1d921fd69b7b808cb
SixMagnitudeStar <95866947+SixMagnitudeStar@users.noreply.github.com>
2025-03-10 10:38:26 +0800

    20250310-2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import EditPanel  from './components/EditPanel.vue';
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <EditPanel msg="RRR"></EditPanel>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
---
+++ src/components/EditPanel.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const iframe = ref(null);
+
+
+const handleClick = ()=>{
+    alert('click喔!');
+    iframe.value.srcdoc = "<h1>Hello, World!</h1><p>This is embedded content.</p>";
+}
+
+const count = ref(0)
+
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+<h1>這是一個可編輯面板xx</h1>
+<p @click="handleClick">測試面板</p>
+<iframe ref="iframe"  id="myiframe" width="600" height="400"></iframe>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
