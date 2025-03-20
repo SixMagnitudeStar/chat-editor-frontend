620258a368fa4e767e4ec689a14df1e97c6734ea
SixMagnitudeStar <plaisir963@gmail.com>
2025-03-21 00:07:45 +0800

    20250320

--- src/components/EditPanel.vue
+++ src/components/EditPanel.vue
@@ -13,19 +13,28 @@ const text1 = ref(null);
 
 const handleClick = async () => {
     alert('click喔!');
-    iframe.value.srcdoc = "<h1>Hello, World!</h1><p>This is embedded content.</p>";
-    text1.value.value = "測試文本ㄅㄅㄅ";
+   // iframe.value.srcdoc = "<h1>Hello, World!</h1><p>This is embedded content.</p>";
+    //text1.value.value = "測試文本ㄅㄅㄅ";
+
+    let prompt = text1.value.value;
+    const encodedPrompt = encodeURIComponent(prompt);
+    alert('輸出值:'+`http://localhost:3000/api/getCode?message=${encodedPrompt}`);
+    console.log(`http://localhost:3000/api/getCode?message=${encodedPrompt}`);
 
     // 發送API請求
     // 發送API請求
     try {
-        const response = await axios.get('http://localhost:3000/generate?message=hi', {
+        const response = await axios.get(`http://localhost:3000/api/getCode?message=${encodedPrompt}`, {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
 
-        text1.value.value = response.data.generatedText;
+       // text1.value.value = response.data.generatedText;
+       console.log('請求');
+
+       iframe.value.srcdoc = response.data.generatedText;
+       code1.value.innerHTML = response.data.generatedText;
 
         // 直接取得回應的資料
         console.log('API response:', response.data);
@@ -33,6 +42,10 @@ const handleClick = async () => {
         console.error('Error fetching API:', error);
     }
 }
+
+
+
+
 
 const count = ref(0)
 
@@ -46,8 +59,8 @@ const count = ref(0)
 <p @click="handleClick">測試面板</p>
 <textarea ref = "text1" >===</textarea>
 <iframe ref="iframe"  id="myiframe" width="600" height="400"></iframe>
-<code>
-   let a = 'vv';
+<code ref="code1">
+
 </code>
 
 </template>
